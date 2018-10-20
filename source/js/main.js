b5a5702d7f2d12c141867b5caa584a38b3d018ce
Alexander Strakhovenko <sasstr@gmail.com>
2018-10-20 03:02:09 +0300

    Пиксель П на моб каÐталога и начало формы так же JS на кноке отк закр меню

--- source/js/main.js
+++ source/js/main.js
@@ -1,18 +1,22 @@
 var navMain = document.querySelectorAll('.header-menu');
 var navToggle = document.querySelector('.header-nav__toggle');
 
-for (var j=0; j < navMain.lenght; j++) {
+for (var j=0; j < navMain.length; j++) {
   navMain[j].classList.remove('header-menu--nojs');
 }
 
 navToggle.addEventListener('click', function() {
-  for (var i=0; i < navMain.lenght; i++){
+  for (var i=0; i < navMain.length; i++){
       if (navMain[i].classList.contains('header-menu--closed')) {
       navMain[i].classList.remove('header-menu--closed');
       navMain[i].classList.add('header-menu--opened');
+      navToggle.classList.remove('header-nav__toggle--closed');
+      navToggle.classList.add('header-nav__toggle--open');
     } else {
       navMain[i].classList.add('header-menu--closed');
       navMain[i].classList.remove('header-menu--opened');
+      navToggle.classList.remove('header-nav__toggle--open');
+      navToggle.classList.add('header-nav__toggle--closed');
     }
   }
 });
