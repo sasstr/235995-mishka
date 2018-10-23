f0965f17054d2152e9383e5b4a8903ede0d2c862
Alexander Strakhovenko <sasstr@gmail.com>
2018-10-23 03:04:14 +0300

    Правки по JS

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,13 @@
 var navMain = document.querySelectorAll('.header-menu');
 var navToggle = document.querySelector('.header-nav__toggle');
 var modal = document.querySelector('.modal');
-var modalWrapper = document.querySelector('.modal__wrapper');
+var orderBtn  = document.querySelector('.thing__order');
+var cartBtn = document.querySelectorAll('.specification__button');
+
+// Работа главного меню
+
+navToggle.classList.remove('header-nav__toggle--nojs');
+navToggle.classList.add('header-nav__toggle--closed');
 
 for (var j=0; j < navMain.length; j++) {
   navMain[j].classList.remove('header-menu--nojs');
@@ -9,7 +15,7 @@ for (var j=0; j < navMain.length; j++) {
 
 navToggle.addEventListener('click', function() {
   for (var i=0; i < navMain.length; i++){
-      if (navMain[i].classList.contains('header-menu--closed')) {
+    if (navMain[i].classList.contains('header-menu--closed')) {
       navMain[i].classList.remove('header-menu--closed');
       navMain[i].classList.add('header-menu--opened');
       navToggle.classList.remove('header-nav__toggle--closed');
@@ -23,20 +29,32 @@ navToggle.addEventListener('click', function() {
   }
 });
 
-/* modal.addEventListener('click', function() {
-  event.preventDefault();
-  var x = event.ClientX;
-  var y = event.ClientY;
-  if(modalWrapper.elementFromPoint(x, y) == undefined){
-  modal.classList.remove('modal--show');
-  modal.classList.add('modal--hidden');
-    }
-  }); */
+// Работа модального окна
 
-var orderBtn  = document.querySelector('.thing__order');
-
-if(orderBtn){ orderBtn.addEventListener('click', function() {
+if(modal){modal.addEventListener('click', function(event) {
   event.preventDefault();
+    if (modal !== event.target) return;
+      modal.classList.remove('modal--show');
+      modal.classList.add('modal--hidden');
+  });
+}
+
+if(cartBtn){
+for(var k=0; k < cartBtn.length ; k++){
+  cartBtn[k].onclick = function(event) {
+    event.preventDefault();
+      if (modal.classList.contains('modal--hidden') ) {
+        modal.classList.remove('modal--hidden');
+        modal.classList.add('modal--show');
+      } else {
+        modal.classList.add('modal--hidden');
+        modal.classList.remove('modal--show');
+      }
+    };
+  }
+}
+if(orderBtn){orderBtn.addEventListener('click', function(evt) {
+  evt.preventDefault();
     if (modal.classList.contains('modal--hidden') ) {
       modal.classList.remove('modal--hidden');
       modal.classList.add('modal--show');
@@ -54,4 +72,3 @@ if (event.keyCode == 27){
   modal.classList.remove('modal--show');
   }
 });
-
